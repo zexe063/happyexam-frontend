7879b7424c364b93e25615a8658be18699d0e60e
murari <zexe063@gmail.com>
2025-01-30 08:34:00 +0530

    nav ui fixed

--- src/landingpage/HomeHeader/HomeHeader.js
+++ src/landingpage/HomeHeader/HomeHeader.js
@@ -1,17 +1,17 @@
  import { HappyexamLogo } from "../../svgicon/icon"
 function HomeHeader(){
     return(
-        <div className=" pl-5 w-full fixed z-50 h-[80px]  border-[1px] border-solid border-x-border_grey shadow-sm flex items-center justify-between  bg-white
+        <nav className="    w-full fixed z-50 h-20  border-[1px] border-solid border-x-border_grey shadow-sm flex items-center justify-between  bg-white
         ">
 
-       <div className= " flex justify-center items-center gap-2 font-bold text-[20px] md:text-[32px] font-Nunito">{HappyexamLogo}HappyExam</div>
+       <div className= " p-3 flex justify-center items-center gap-2 font-bold text-xl md:text-2xl font-Nunito">{HappyexamLogo}<span>HappyExam</span></div>
 
   <div className=" flex justify-center items-center gap-2 p-5">
-    <div className=" w-[80px] h-[40px] md:w-[100px] md:h-[50px] rounded-full flex justify-center items-center border-[2px] border-solid border-x-border_grey font-Nunito text-black">Log in</div>
-  <div className=" w-[100px] h-[40px] md:w-[120px] md:h-[50px] rounded-full bg-button_green text-white flex justify-center items-center font-semibold font-Nunito " >Get Start</div>
+    <div className=" px-3 py-2 md:px-8 md:py-3 rounded-full flex justify-center items-center border-[2px] border-solid border-x-border_grey font-Nunito text-black">Log in</div>
+  <div className=" px-3 py-2 md:px-8 md:py-3 rounded-full bg-button_green text-white flex justify-center items-center font-semibold font-Nunito " >Get Start</div>
   </div>
 
-        </div>
+        </nav>
     )
 }
 
