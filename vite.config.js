e3783ca9dfcac34ffe3122c6acae6e592c0ed110
murari <zexe063@gmail.com>
2025-02-16 08:50:18 +0530

    buil config done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build:{
+    outDir:"build",
+    assetsDir:"assets"
+  }
  
 })
